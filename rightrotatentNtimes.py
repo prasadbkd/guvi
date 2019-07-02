80717964f7c072ee5684430ccdffa1389305b024
Prasad BKD <48713377+prasadbkd@users.noreply.github.com>
2019-07-02 09:37:59 +0530

    Update rightrotatentNtimes.py

--- rightrotatentNtimes.py
+++ rightrotatentNtimes.py
@@ -2,5 +2,7 @@ n,k = map(int,input().split())
   
 list_1 = [x for x in input().split()] 
 list_1 = (list_1[-k:] + list_1[:-k]) 
+if len(list_1)<=2:
+    list_1.reverse()
 print(" ".join(list_1)) 
 #prasad
